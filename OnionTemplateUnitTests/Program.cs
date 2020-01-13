02963ffe2476a3e4e48057514702e93cee8ba41f
JacobS219 <jschinel@gmail.com>
2020-01-12 18:50:16 -0600

    Initial Commit

---
+++ OnionTemplateUnitTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OnionTemplateUnitTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
